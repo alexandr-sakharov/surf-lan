e54c7a4fbc96e6fe6e134014b1b00bc7ce5d6505
SAXAR17 <alexandr3554@mail.ru>
2022-02-20 20:39:00 +0300

    complete project: add project git

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$(function () {
+  $(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    autoplay: true,
+  autoplaySpeed: 10000,
+    prevArrow:
+      '<img class="slider-arrows slick-arrows__left" src="img/arrows-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slick-arrows__right" src="img/arrows-right.svg" alt="" />',
+    asNavFor: ".slider-dotshead",
+  });
+  $(".slider-dotshead").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".header__slider",
+  });
+
+  $('.surf__slider').slick({
+    centerMode: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slick-arrows__left" src="img/arrows-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slick-arrows__right" src="img/arrows-right.svg" alt="" />',
+      asNavFor: '.slider-map'
+  });
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf__slider',
+    focusOnSelect: true
+  });
+  $('.travel__slider, .shop__slider').slick({
+infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slick-arrows__left" src="img/arrows-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slick-arrows__right" src="img/arrows-right.svg" alt="" />',
+  });
+
+  $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="./img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="./img/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+    $('.quantity').each(function() {
+      var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+
+    $('.quantity-button').on('click', function(){
+      summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val()-1) * $('.summ').data('guests');
+      $('.summ').html('$' + summ);
+    });
+    summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val()-1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ);
+
+    $('.surfboard-box__circle').on('click',function(){
+      $(this).toggleClass('active');
+    });
+
+    new WOW().init();
+});
